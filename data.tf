3b55f979cc1e6967bdd44533ecbe1a28a0369280
Sebastian Struß <31846129+applike-ss@users.noreply.github.com>
2023-09-06 12:01:57 +0200

    fix: family usage fixed (#38)
    ## Description
    <!--- Describe your changes in detail -->
    
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## Breaking Changes
    <!-- Does this break backwards compatibility with the current major
    version? -->
    <!-- If so, please provide an explanation why it is necessary. -->
    
    ## How Has This Been Tested?
    - [ ] I have updated at least one of the `examples/*` to demonstrate and
    validate my change(s)
    - [ ] I have tested and validated these changes using one or more of the
    provided `examples/*` projects
    <!--- Users should start with an existing example as its written, deploy
    it, then check their changes against it -->
    <!--- This will highlight breaking/disruptive changes. Once you have
    checked, deploy your changes to verify -->
    <!--- Please describe how you tested your changes -->
    - [ ] I have executed `pre-commit run -a` on my pull request
    <!--- Please see
    https://github.com/antonbabenko/pre-commit-terraform#how-to-install for
    how to install -->

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "gosoline_application_metadata_definition" "main" {
   count       = local.alarm_enabled
   project     = module.this.namespace
   environment = module.this.environment
-  family      = module.this.namespace
+  family      = module.this.tenant
   application = module.this.name
   group       = module.this.stage
 }
@@ -11,7 +11,7 @@ data "gosoline_application_dashboard_definition" "main" {
   count       = local.grafana_dashboard_create
   project     = module.this.namespace
   environment = module.this.environment
-  family      = module.this.namespace
+  family      = module.this.tenant
   application = module.this.name
   containers  = local.containers
   group       = module.this.stage
