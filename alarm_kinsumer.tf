8f08d33ea18c2e72f34f1f50ba6977bae7091ca7
Sebastian Struß <struss@justtrack.io>
2024-05-07 15:31:45 +0200

    fix: kinsumers with same name crashed module

--- alarm_kinsumer.tf
+++ alarm_kinsumer.tf
@@ -1,7 +1,14 @@
-module "alarm_kinsumer" {
-  for_each = { for kinsumer in length(data.gosoline_application_metadata_definition.main) > 0 ? element(data.gosoline_application_metadata_definition.main, 0).metadata.cloud.aws.kinesis.kinsumers : [] : kinsumer.name => {
+locals {
+  kinsumers = { for kinsumer in length(data.gosoline_application_metadata_definition.main) > 0 ? element(data.gosoline_application_metadata_definition.main, 0).metadata.cloud.aws.kinesis.kinsumers : [] : kinsumer.name => {
     metadata = kinsumer
-  } }
+  }... }
+  kinsumer_keys = keys(local.kinsumers)
+  # There can be more than one kinsumer with the same name if the application is not configured correctly. The terraform module should not fail due to the app not being configured correctly though.
+  kinsumer_metadatas = { for kinsumer_key in local.kinsumer_keys : kinsumer_key => local.kinsumers[kinsumer_key][0] }
+}
+
+module "alarm_kinsumer" {
+  for_each = local.kinsumer_metadatas
 
   source  = "justtrackio/ecs-alarm-kinsumer/aws"
   version = "1.0.1"
