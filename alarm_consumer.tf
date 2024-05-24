aef5665a0925729cd319eca9db338c218ba2c2f1
Sebastian Elm <sebastian@justtrack.io>
2024-05-24 09:56:55 +0200

    fix: use maps for alarm overwrites to correctly target individual alarms;

--- alarm_consumer.tf
+++ alarm_consumer.tf
@@ -1,21 +1,38 @@
+locals {
+  consumers = length(data.gosoline_application_metadata_definition.main) > 0 ? element(data.gosoline_application_metadata_definition.main, 0).metadata.stream.consumers : []
+  default_alarm_consumer = {
+    for consumer in local.consumers : consumer.name => {
+      # defaults should be the same as in justtrackio/ecs-alarm-consumer/aws
+      alarm_description      = null
+      datapoints_to_alarm    = 3
+      evaluation_periods     = 3
+      period                 = 60
+      success_rate_threshold = 99
+    }
+  }
+  alarm_consumer = merge(local.default_alarm_consumer, var.alarm_consumer)
+}
+
 module "alarm_consumer" {
-  for_each = { for consumer in length(data.gosoline_application_metadata_definition.main) > 0 ? element(data.gosoline_application_metadata_definition.main, 0).metadata.stream.consumers : [] : consumer.name => {
-    metadata = consumer
-  } }
+  for_each = {
+    for consumer in local.consumers : consumer.name => {
+      metadata = consumer
+    }
+  }
 
   source  = "justtrackio/ecs-alarm-consumer/aws"
   version = "1.1.0"
 
   alarm_description = jsonencode(merge({
     Severity    = "warning"
-    Description = var.alarm_consumer.alarm_description
+    Description = local.alarm_consumer[each.key].alarm_description
   }, module.this.tags, module.this.additional_tag_map))
   alarm_topic_arn     = data.aws_sns_topic.default.arn
   consumer_name       = each.value.metadata.name
-  datapoints_to_alarm = var.alarm_consumer.datapoints_to_alarm
-  evaluation_periods  = var.alarm_consumer.evaluation_periods
-  period              = var.alarm_consumer.period
-  threshold           = var.alarm_consumer.success_rate_threshold
+  datapoints_to_alarm = local.alarm_consumer[each.key].datapoints_to_alarm
+  evaluation_periods  = local.alarm_consumer[each.key].evaluation_periods
+  period              = local.alarm_consumer[each.key].period
+  threshold           = local.alarm_consumer[each.key].success_rate_threshold
 
   label_orders = var.label_orders
   context      = module.this.context
--- alarm_kinsumer.tf
+++ alarm_kinsumer.tf
@@ -5,6 +5,17 @@ locals {
   kinsumer_keys = keys(local.kinsumers)
   # There can be more than one kinsumer with the same name if the application is not configured correctly. The terraform module should not fail due to the app not being configured correctly though.
   kinsumer_metadatas = { for kinsumer_key in local.kinsumer_keys : kinsumer_key => local.kinsumers[kinsumer_key][0] }
+  default_alarm_kinsumer = {
+    for kinsumer in local.kinsumer_metadatas : kinsumer.metadata.name => {
+      # defaults should be the same as in the justtrackio/ecs-alarm-kinsumer/aws
+      alarm_description        = null
+      datapoints_to_alarm      = 1
+      evaluation_periods       = 1
+      period                   = 60
+      threshold_seconds_behind = 3600
+    }
+  }
+  alarm_kinsumer = merge(local.default_alarm_kinsumer, var.alarm_kinsumer)
 }
 
 module "alarm_kinsumer" {
@@ -15,15 +26,15 @@ module "alarm_kinsumer" {
 
   alarm_description = jsonencode(merge({
     Severity    = "warning"
-    Description = var.alarm_kinsumer.alarm_description
+    Description = local.alarm_kinsumer[each.key].alarm_description
   }, module.this.tags, module.this.additional_tag_map))
   alarm_topic_arn      = data.aws_sns_topic.default.arn
-  datapoints_to_alarm  = var.alarm_kinsumer.datapoints_to_alarm
-  evaluation_periods   = var.alarm_kinsumer.evaluation_periods
+  datapoints_to_alarm  = local.alarm_kinsumer[each.key].datapoints_to_alarm
+  evaluation_periods   = local.alarm_kinsumer[each.key].evaluation_periods
   kinsumer_name        = each.value.metadata.name
   kinsumer_stream_name = each.value.metadata.stream_name_full
-  period               = var.alarm_kinsumer.period
-  threshold            = var.alarm_kinsumer.threshold_seconds_behind
+  period               = local.alarm_kinsumer[each.key].period
+  threshold            = local.alarm_kinsumer[each.key].threshold_seconds_behind
 
   label_orders = var.label_orders
   context      = module.this.context
