5d89e738b7f7a86d88f93d2e7d74a95a36883e67
Bünyamin Dökmetaş <19335284+ztzxt@users.noreply.github.com>
2025-09-22 17:32:54 +0300

    fix: exclude 6.14.0 from aws provider version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.67, != 5.71.0"
+      version = ">= 4.67, != 6.14.0"
     }
 
     gosoline = {
